6e9e7a7f2aa41a1671a90ab622572d228c832596
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2023-12-15 21:17:05 +0600

    avates ternary operator is used

--- src/components/Shared/Navbar/Avatar.jsx
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AvatarImg from '../../../assets/images/placeholder.jpg'
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Avatar = () => {
+
+    const { user } = useContext(AuthContext)
     return (
-        <img className='rounded-full' src={AvatarImg} alt="profile" height='30' width='30' />
+        <img className='rounded-full' 
+        src={user && user.photoURL ? user.photoURL: AvatarImg} alt="profile" 
+        height='30' width='30' />
     );
 };
 
