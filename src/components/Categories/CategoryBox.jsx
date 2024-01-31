32149e0c378c1ad83570f0849896ee0180cd701c
iqbalhossainmohon <iqbalhossainmohon3057@gamil.com>
2024-01-31 12:50:16 +0600

    card added

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CategoryBox = ({label, icon: Icon}) => {
     return (
-        <div className='flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 '>
+        <div className='flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer'>
             <Icon size={26}/>
             <div className='text-sm font-medium'>{label}</div>
         </div>
